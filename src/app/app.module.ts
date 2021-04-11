54bf1a163e713f8291715dcb50712e811d4ab366
agamble8 <amymgamble@gmail.com>
2021-04-10 20:12:12 -0500

    added an about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
 import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 const routes: Route[] = [
   { path: 'trivia', component: TriviaComponent },
-  { path: '', component: TriviaComponent},
-  { path: '**', redirectTo: ''}
+  { path: 'about', component: AboutComponent },
+  { path: '', component: TriviaComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TriviaComponent
+    TriviaComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
